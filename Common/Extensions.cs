c7097d7a361567f2f8146791aaeef4f6e8c4f416
Mikko Heikela <mikko.heikela@gmail.com>
2021-12-04 06:17:04 +0000

    Day 4 solution

---
+++ Common/Extensions.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace Common
+{
+    public static class Extensions
+    {
+        public static IEnumerable<List<TElem>> SplitWhen<TElem>(this IEnumerable<TElem> seq,
+            Func<TElem, TElem, bool> splitPredicate)
+        {
+            List<TElem> prefix = new List<TElem>();
+            foreach (TElem next in seq)
+            {
+                if (prefix.Any())
+                {
+                    if (splitPredicate(prefix.Last(), next))
+                    {
+                        yield return prefix;
+                        prefix = new List<TElem>();
+                    }
+                }
+                prefix.Add(next);
+            }
+            if (prefix.Any())
+            {
+                yield return prefix;
+            }
+            yield break;
+        }
+
+        public static IEnumerable<List<TElem>> SplitBy<TElem>(this IEnumerable<TElem> seq,
+            Func<TElem, bool> separatorPredicate)
+        {
+            List<TElem> prefix = new List<TElem>();
+            foreach (TElem next in seq)
+            {
+                if (separatorPredicate(next))
+                {
+                    if (prefix.Any())
+                    {
+                        yield return prefix;
+                        prefix = new List<TElem>();
+                    }
+                }
+                else
+                {
+                    prefix.Add(next);
+                }
+            }
+            if (prefix.Any())
+            {
+                yield return prefix;
+            }
+            yield break;
+        }
+
+        public static IEnumerable<List<string>> Paragraphs(this IEnumerable<string> lines)
+        {
+            return lines.SplitBy(s => s.Trim().Length == 0);
+        }
+
+        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> keyValuePairs)
+        {
+            return keyValuePairs.ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+        public static TVal GetOrElse<TKey, TVal>(this Dictionary<TKey, TVal> dict, TKey key, TVal ifNotFound)
+        {
+            if (dict.TryGetValue(key, out TVal result))
+            {
+                return result;
+            }
+            else
+            {
+                return ifNotFound;
+            }
+        }
+
+        public static IEnumerable<KeyValuePair<int, T>> ZipWithIndex<T>(this IEnumerable<T> src)
+        {
+            int i = 0;
+            foreach (T e in src)
+            {
+                yield return KeyValuePair.Create(i++, e);
+            }
+            yield break;
+        }
+
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> src)
+        {
+            return src.SelectMany(part => part);
+        }
+
+    }
+}
---
+++ Day04/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Common;
+
+Func<List<string>, List<List<int>>> parseBoard = lines =>
+{
+    var rows = lines.Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()).ToList();
+    var columns = new List<List<int>>();
+    for (int pos = 0; pos < rows[0].Count; pos++)
+    {
+        var column = new List<int>();
+        for (int row = 0; row < rows.Count; row++)
+        {
+            column.Add(rows[row][pos]);
+        }
+    }
+    return rows.Concat(columns).ToList();
+};
+
+Func<List<List<int>>, HashSet<int>, Boolean> isWinning = (board, called) => board.Any(rowOrColumn => rowOrColumn.All(number => called.Contains(number)));
+
+Func<List<List<int>>, HashSet<int>, int> uncalled = (board, called) => board.Flatten().ToHashSet().Except(called).ToList().Sum();
+
+var lines = File.ReadLines("input01.txt").ToList();
+
+var callSequence = lines[0].Split(',').Select(int.Parse);
+
+var boardInputs = lines.Skip(1).Paragraphs();
+
+var boards = boardInputs.Select(parseBoard);
+
+var called = new HashSet<int>();
+
+foreach (int call in callSequence)
+{
+    called.Add(call);
+    var winning = boards.Where(b => isWinning(b, called)).ToList();
+    foreach(var board in winning)
+    {
+        Console.WriteLine($"Call {call}, Winning Score: {uncalled(board, called) * call}");
+    }
+    boards = boards.Where(b => !winning.Contains(b)).ToList();
+}
