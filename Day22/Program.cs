39b7f68d3d1e1201e8cdfab6f4de43ba30c97750
Mikko Heikela <mikko.heikela@gmail.com>
2021-12-22 06:09:42 +0000

    Day 22 part 2, attempting a semi brute force

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -51,6 +51,93 @@ foreach (var instruction in instructions)
 
 Console.WriteLine(part1State.Count());
 
+HashSet<int> xDividers = new HashSet<int>();
+HashSet<int> yDividers = new HashSet<int>();
+HashSet<int> zDividers = new HashSet<int>();
+
+foreach (Instruction instruction in instructions)
+{
+    xDividers.Add(instruction.area.min.x);
+    xDividers.Add(instruction.area.max.x + 1);
+    yDividers.Add(instruction.area.min.y);
+    yDividers.Add(instruction.area.max.y + 1);
+    zDividers.Add(instruction.area.min.z);
+    zDividers.Add(instruction.area.max.z + 1);
+}
+
+HashSet<Point> rectanglesOn = new HashSet<Point>();
+
+
+
+foreach (Instruction instruction in instructions)
+{
+    List<int> xAffected = new List<int>();
+    List<int> yAffected = new List<int>();
+    List<int> zAffected = new List<int>();
+    for (int x = instruction.area.min.x; x <= instruction.area.max.x; ++x)
+    {
+        if (xDividers.Contains(x))
+        {
+            xAffected.Add(x);
+        }
+    }
+    for (int y = instruction.area.min.y; y <= instruction.area.max.y; ++y)
+    {
+        if (yDividers.Contains(y))
+        {
+            yAffected.Add(y);
+        }
+    }
+    for (int z = instruction.area.min.z; z <= instruction.area.max.z; ++z)
+    {
+        if (zDividers.Contains(z))
+        {
+            zAffected.Add(z);
+        }
+    }
+    foreach (int x in xAffected)
+    {
+        foreach (int y in yAffected)
+        {
+            foreach (int z in zAffected)
+            {
+                Point pos = new Point(x, y, z);
+                if (instruction.on)
+                {
+                    rectanglesOn.Add(pos);
+                }
+                else
+                {
+                    rectanglesOn.Remove(pos);
+                }
+            }
+        }
+    }
+}
+
+List<int> xDivSorted = xDividers.OrderBy(x => x).ToList();
+List<int> yDivSorted = yDividers.OrderBy(x => x).ToList();
+List<int> zDivSorted = zDividers.OrderBy(x => x).ToList();
+
+long onCount = 0;
+
+for (int xi = 0; xi < xDivSorted.Count - 1; ++xi)
+{
+    for (int yi = 0; yi < yDivSorted.Count - 1; ++yi)
+    {
+        for (int zi = 0; zi < xDivSorted.Count - 1; ++zi)
+        {
+            Point pos = new Point(xDivSorted[xi], yDivSorted[yi], zDivSorted[zi]);
+            if (rectanglesOn.Contains(pos))
+            {
+                onCount += xDivSorted[xi + 1] - xDivSorted[xi] * yDivSorted[yi + 1] - yDivSorted[yi] * zDivSorted[zi + 1] - zDivSorted[zi];
+            }
+        }
+    }
+}
+
+Console.WriteLine(onCount);
+
 public record Point(int x, int y, int z);
 
 public record Rectangle(Point min, Point max)
