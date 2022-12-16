c43b6ed8499ff188a9c26dfd838d1a9cce2823c6
Mikko Heikela <mikko.heikela@gmail.com>
2022-12-16 06:22:06 +0000

    Day 16 part 1 and Day 15

---
+++ Day15-22/Program.cs
@@ -0,0 +1,131 @@
+﻿var input = File.ReadAllLines("../../../input.txt");
+//var input = File.ReadAllLines("../../../testInput.txt");
+
+//int relevantLine = 10;
+int relevantLine = 2000000;
+
+List<Span> spansOnLine = new List<Span>();
+
+HashSet<Point> beacons = new HashSet<Point>();
+
+Dictionary<Point, int> exclusions = new Dictionary<Point, int>();
+
+foreach (string line in input)
+{
+    string[] parts = line.Split(' ');
+    Point sensor = new Point(int.Parse(parts[2].Substring(2, parts[2].Length - 3)), int.Parse(parts[3].Substring(2, parts[3].Length - 3)));
+    Point beacon = new Point(int.Parse(parts[8].Substring(2, parts[8].Length - 3)), int.Parse(parts[9].Substring(2)));
+
+    beacons.Add(beacon);
+
+    int dist = sensor.ManhattanDist(beacon);
+
+    exclusions.Add(sensor, dist);
+
+    int spanStart = sensor.x - dist + Math.Abs(sensor.y - relevantLine);
+    int spanEnd = sensor.x + dist - Math.Abs(sensor.y - relevantLine);
+    
+    if (spanStart <= spanEnd)
+    {
+        spansOnLine.Add(new Span(spanStart, spanEnd));
+    }
+}
+
+HashSet<int> covered = new HashSet<int>();
+
+foreach (Span span in spansOnLine)
+{
+    for (int x = span.first; x <= span.last; x++)
+    {
+        covered.Add(x);
+    }
+}
+
+foreach (Point beacon in beacons)
+{
+    if (beacon.y == relevantLine)
+    {
+        if (covered.Contains(beacon.x))
+        {
+            covered.Remove(beacon.x);
+        }
+    }
+}
+
+Console.WriteLine(covered.Count);
+
+//int searchSize = 20;
+int searchSize = 4000000;
+
+for (int y = 0; y <= searchSize; ++y)
+{
+    if (y % 100000 == 0)
+    {
+        Console.Write('.');
+    }
+    for (int x = 0; x <= searchSize;)
+    {
+        int nextX = x;
+        foreach (var beacon in exclusions)
+        {
+            if (beacon.Key.x - beacon.Value + Math.Abs(beacon.Key.y - y) <= x)
+            {
+                int pushedX = beacon.Key.x + beacon.Value - Math.Abs(beacon.Key.y - y) + 1;
+                if (pushedX > nextX)
+                {
+                    nextX = pushedX;
+                }
+            }
+        }
+        if (nextX == x)
+        {
+            Console.WriteLine();
+            long tuning = x;
+            tuning = tuning * 4000000;
+            tuning = tuning + y;
+            Console.WriteLine(tuning);
+            goto Done;
+        }
+        x = nextX;
+    }
+}
+Done:
+Console.WriteLine();
+
+public record Span(int first, int last)
+{
+    public bool Overlaps(Span other)
+    {
+        if (this.last >= other.first && this.first <= other.last)
+        {
+            return true;
+        }
+        if (other.last >= this.first && other.first <= this.last)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public Span Combine(Span other)
+    {
+        if (!Overlaps(other))
+        {
+            throw new ArgumentException("Cannot merge with non-overlapping span");
+        }
+        return new Span(Math.Min(first, other.first), Math.Max(last, other.last));
+    }
+}
+
+public record Point(int x, int y)
+{
+    public static Point operator +(Point a, Point b) => new Point(a.x + b.x, a.y + b.y);
+    public static Point operator -(Point a, Point b) => new Point(a.x - b.x, a.y - b.y);
+
+    public int ManhattanDist(Point other)
+    {
+        Point d = other - this;
+        return Math.Abs(d.x) + Math.Abs(d.y);
+    }
+}
+
---
+++ Day16-22/Program.cs
@@ -0,0 +1,87 @@
+﻿using Common;
+using Priority_Queue;
+
+
+//var input = File.ReadAllLines("../../../testInput.txt");
+var input = File.ReadAllLines("../../../input.txt");
+
+ConcreteGraph<string> allNodes = new ConcreteGraph<string>();
+Dictionary<string, int> workingValves = new Dictionary<string, int>();
+ConcreteWeightedGraph<string> interestingNodes = new ConcreteWeightedGraph<string>();
+Dictionary<(string, string), int> interestingDistances = new Dictionary<(string, string), int>();
+
+foreach (var line in input)
+{
+    string[] parts = line.Split(' ');
+    string node = parts[1];
+    string flowDesc = parts[4];
+    int flow = int.Parse(flowDesc.Substring(5, flowDesc.Length - 6));
+
+    if (flow > 0)
+    {
+        workingValves[node] = flow;
+    }
+
+    var destinations = parts.Skip(9);
+
+    foreach (var dest in destinations)
+    {
+        string destName = dest.Substring(0, 2);
+        allNodes.AddEdge(node, destName);
+        allNodes.AddEdge(destName, node);
+    }
+}
+
+List<string> interesting = workingValves.Keys.ToList();
+interesting.Add("AA");
+
+for (int i = 0; i < interesting.Count; i++)
+{
+    for (int j = i + 1; j < interesting.Count; j++)
+    {
+        int dist = allNodes.ShortestPathTo(interesting[i], interesting[j]).GetLength();
+        interestingNodes.AddEdge(interesting[i], interesting[j], dist);
+        interestingNodes.AddEdge(interesting[j], interesting[i], dist);
+        interestingDistances.Add((interesting[i], interesting[j]), dist);
+        interestingDistances.Add((interesting[j], interesting[i]), dist);
+    }
+}
+
+interesting.Remove("AA");
+
+SimplePriorityQueue<State> options = new SimplePriorityQueue<State>();
+
+options.Enqueue(new State(interesting, "AA", 30, 0, 0, 0), 0);
+
+State CalculateMove(State current, string chosen)
+{
+    int dist = interestingDistances[(current.current, chosen)];
+    int timeSpent = dist + 1;
+    int newTimeLeft = current.timeLeft - timeSpent;
+    int newReleasing = current.releasing + workingValves[chosen];
+    int released = current.released + timeSpent * current.releasing;
+    int forecast = released + newTimeLeft * newReleasing;
+    List<string> newCandidates = new List<string>(current.candidates);
+    newCandidates.Remove(chosen);
+    return new State(newCandidates, chosen, newTimeLeft, newReleasing, released, forecast);
+}
+
+int best = 0;
+while (options.Count > 0)
+{
+    State currentState = options.Dequeue();
+
+    IEnumerable<State> possibleStates = currentState.candidates.Select(x => CalculateMove(currentState, x)).Where(s => s.timeLeft >= 0);
+    foreach (var move in possibleStates)
+    {
+        options.Enqueue(move, -move.forecast);
+        if (move.forecast > best)
+        {
+            best = move.forecast;
+            Console.WriteLine($"We can do {best}");
+        }
+    }
+}
+
+public record State(List<string> candidates, string current, int timeLeft, int releasing, int released, int forecast);
+
