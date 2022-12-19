ac9d0eca911cc797ef8bb7d275e48f92b430d43c
Mikko Heikela <mikko.heikela@gmail.com>
2022-12-19 07:12:15 +0000

    Day 19 part one and false starts on part 2

---
+++ Day19-22/Program.cs
@@ -0,0 +1,268 @@
+﻿var input = File.ReadAllLines("../../../input.txt");
+//var input = File.ReadAllLines("../../../testInput.txt");
+
+List<Blueprint> blueprints = input.Select(Blueprint.Parse).ToList();
+
+/*
+IEnumerable<State> PossibleSuccessors(State current, Blueprint blueprint)
+{
+    if (current.time == maxTime)
+    {
+        yield break;
+    }
+    Stock newMaterials = current.materials + current.bots;
+    int newTime = current.time + 1;
+    if (current.materials.GreaterOrEqual(blueprint.geodecost) && current.materials.EqualOnNonZeroComponent(blueprint.geodecost))
+    {
+        yield return new State(current.bots + new Stock(0, 0, 0, 1), newMaterials - blueprint.geodecost, newTime);
+    }
+    if (current.materials.GreaterOrEqual(blueprint.obsidiancost) && current.materials.EqualOnNonZeroComponent(blueprint.obsidiancost))
+    {
+        yield return new State(current.bots + new Stock(0, 0, 1, 0), newMaterials - blueprint.obsidiancost, newTime);
+    }
+    if (current.materials.GreaterOrEqual(blueprint.claycost) && current.materials.EqualOnNonZeroComponent(blueprint.claycost))
+    {
+        yield return new State(current.bots + new Stock(0, 1, 0, 0), newMaterials - blueprint.claycost, newTime);
+    }
+    if (current.materials.GreaterOrEqual(blueprint.orecost) && current.materials.EqualOnNonZeroComponent(blueprint.orecost))
+    {
+        yield return new State(current.bots + new Stock(1, 0, 0, 0), newMaterials - blueprint.orecost, newTime);
+    }
+    if (!(current.materials.GreaterOrEqual(blueprint.geodecost) &&
+        current.materials.GreaterOrEqual(blueprint.obsidiancost) &&
+        current.materials.GreaterOrEqual(blueprint.claycost) &&
+        current.materials.GreaterOrEqual(blueprint.orecost)))
+    {
+        yield return new State(current.bots, newMaterials, newTime);
+    }
+}
+*/
+
+IEnumerable<State> PossibleSuccessors2(State current, Blueprint blueprint, int maxTime)
+{
+    if (current.time == maxTime)
+    {
+        yield break;
+    }
+    int maxOreNeed = Math.Max(blueprint.orecost.ore, Math.Max(blueprint.claycost.ore, Math.Max(blueprint.obsidiancost.ore, blueprint.geodecost.ore)));
+    int maxClayNeed = blueprint.obsidiancost.clay;
+    int maxObsidianNeed = blueprint.geodecost.obsidian;
+
+    // Build no more robots
+    yield return new State(current.bots, current.materials + current.bots * (maxTime - current.time), maxTime);
+    // Build an ore bot next
+    if (current.bots.ore < maxOreNeed)
+    {
+        int newTime = current.time;
+        Stock newMaterials = current.materials;
+        while (!newMaterials.GreaterOrEqual(blueprint.orecost))
+        {
+            newTime++;
+            newMaterials = newMaterials + current.bots;
+        }
+        newTime++;
+        newMaterials += current.bots;
+        newMaterials -= blueprint.orecost;
+        if (newTime <= maxTime)
+        {
+            yield return new State(current.bots + new Stock(1, 0, 0, 0), newMaterials, newTime);
+        }
+    }
+    // Build a clay bot next
+    if (current.bots.clay < maxClayNeed)
+    {
+        int newTime = current.time;
+        Stock newMaterials = current.materials;
+        while (!newMaterials.GreaterOrEqual(blueprint.claycost))
+        {
+            newTime++;
+            newMaterials = newMaterials + current.bots;
+        }
+        newTime++;
+        newMaterials += current.bots;
+        newMaterials -= blueprint.claycost;
+        if (newTime <= maxTime)
+        {
+            yield return new State(current.bots + new Stock(0, 1, 0, 0), newMaterials, newTime);
+        }
+    }
+    // Build an obsidian bot next
+    if (current.bots.clay > 0 && current.bots.obsidian < maxObsidianNeed)
+    {
+        int newTime = current.time;
+        Stock newMaterials = current.materials;
+        while (!newMaterials.GreaterOrEqual(blueprint.obsidiancost))
+        {
+            newTime++;
+            newMaterials = newMaterials + current.bots;
+        }
+        newTime++;
+        newMaterials += current.bots;
+        newMaterials -= blueprint.obsidiancost;
+        if (newTime <= maxTime)
+        {
+            yield return new State(current.bots + new Stock(0, 0, 1, 0), newMaterials, newTime);
+        }
+    }
+    // Build a geode bot next
+    if (current.bots.obsidian > 0)
+    {
+        int newTime = current.time;
+        Stock newMaterials = current.materials;
+        while (!newMaterials.GreaterOrEqual(blueprint.geodecost))
+        {
+            newTime++;
+            newMaterials = newMaterials + current.bots;
+        }
+        newTime++;
+        newMaterials += current.bots;
+        newMaterials -= blueprint.geodecost;
+        if (newTime <= maxTime)
+        {
+            yield return new State(current.bots + new Stock(0, 0, 0, 1), newMaterials, newTime);
+        }
+    }
+}
+
+int MostGeodes(Blueprint blueprint, int maxTime)
+{
+    State start = new State(new Stock(1, 0, 0, 0), new Stock(0, 0, 0, 0), 0);
+
+    Queue<State> search = new Queue<State>();
+    HashSet<State> visited = new HashSet<State>();
+    search.Enqueue(start);
+
+    int mostGeodes = 0;
+
+    while (search.Count > 0)
+    {
+        State current = search.Dequeue();
+        visited.Add(current);
+        if (current.materials.geodes > mostGeodes)
+        {
+            mostGeodes = current.materials.geodes;
+        }
+        foreach (State s in PossibleSuccessors2(current, blueprint, maxTime))
+        {
+            if (!visited.Contains(s))
+            {
+                search.Enqueue(s);
+            }
+        }
+    }
+    return mostGeodes;
+}
+
+int BlueprintQuality(Blueprint blueprint, int maxTime)
+{
+    return MostGeodes(blueprint, maxTime) * blueprint.number;
+}
+
+int BlueprintQuality2(Blueprint blueprint, int maxTime)
+{
+    return MostGeodes2(blueprint, maxTime) * blueprint.number;
+}
+
+int MostGeodes2(Blueprint blueprint, int maxTime)
+{
+    int mostGeodes = 0;
+
+    int maxOreNeed = Math.Max(blueprint.orecost.ore, Math.Max(blueprint.claycost.ore, Math.Max(blueprint.obsidiancost.ore, blueprint.geodecost.ore)));
+    int maxClayNeed = blueprint.obsidiancost.clay;
+    int maxObsidianNeed = blueprint.geodecost.obsidian;
+
+    for (int oreBotTarget = 1; oreBotTarget <= maxOreNeed; ++oreBotTarget)
+    {
+        for (int clayBotTarget = 1; clayBotTarget <= maxClayNeed; ++clayBotTarget)
+        {
+            for (int obsidianBotTarget = 1; obsidianBotTarget <= maxOreNeed; ++obsidianBotTarget)
+            {
+                State start = new State(new Stock(1, 0, 0, 0), new Stock(0, 0, 0, 0), 0);
+                State current = start;
+                while (current.time < maxTime)
+                {
+                    if (current.materials.GreaterOrEqual(blueprint.geodecost))
+                    {
+                        current = new State(current.bots + new Stock(0, 0, 0, 1), current.materials + current.bots - blueprint.geodecost, current.time + 1);
+                    }
+                    else if (current.materials.GreaterOrEqual(blueprint.orecost) && current.bots.ore < oreBotTarget)
+                    {
+                        current = new State(current.bots + new Stock(1, 0, 0, 0), current.materials + current.bots - blueprint.orecost, current.time + 1);
+                    }
+                    else if (current.materials.GreaterOrEqual(blueprint.obsidiancost) && current.bots.obsidian < obsidianBotTarget)
+                    {
+                        current = new State(current.bots + new Stock(0, 0, 1, 0), current.materials + current.bots - blueprint.obsidiancost, current.time + 1);
+                    }
+                    else if (current.materials.GreaterOrEqual(blueprint.claycost) && current.bots.clay < clayBotTarget)
+                    {
+                        current = new State(current.bots + new Stock(0, 1, 0, 0), current.materials + current.bots - blueprint.claycost, current.time + 1);
+                    }
+                    else
+                    {
+                        current = new State(current.bots, current.materials + current.bots, current.time + 1);
+                    }
+                }
+                if (current.materials.geodes > mostGeodes)
+                {
+                    mostGeodes = current.materials.geodes;
+                }
+            }
+        }
+    }
+
+    return mostGeodes;
+}
+
+int totalQualities = blueprints.Select(b => BlueprintQuality(b, 24)).Sum();
+
+Console.WriteLine($"Sum of qualities = {totalQualities}");
+
+totalQualities = blueprints.Select(b => BlueprintQuality2(b, 24)).Sum();
+
+Console.WriteLine($"Sum of qualities = {totalQualities}");
+
+Console.WriteLine($"Part 2 = {blueprints.Take(3).Select(b => MostGeodes2(b, 33)).Aggregate((a, b) => a * b)}");
+
+record Stock(int ore, int clay, int obsidian, int geodes)
+{
+    public static Stock operator +(Stock a, Stock b) => new Stock(a.ore + b.ore, a.clay + b.clay, a.obsidian + b.obsidian, a.geodes + b.geodes);
+    public static Stock operator -(Stock a, Stock b) => new Stock(a.ore - b.ore, a.clay - b.clay, a.obsidian - b.obsidian, a.geodes - b.geodes);
+
+    public static Stock operator *(int b, Stock a) => new Stock(a.ore * b, a.clay * b, a.obsidian * b, a.geodes * b);
+    public static Stock operator *(Stock a, int b) => b * a;
+
+    public bool GreaterOrEqual(Stock other)
+    {
+        return ore >= other.ore && clay >= other.clay && obsidian >= other.obsidian && geodes >= other.geodes;
+    }
+
+    public bool EqualOnNonZeroComponent(Stock other)
+    {
+        if (ore >= 0 && ore == other.ore) return true;
+        if (clay >= 0 && clay == other.clay) return true;
+        if (obsidian >= 0 && obsidian == other.obsidian) return true;
+        if (geodes >= 0 && geodes == other.geodes) return true;
+        return false;
+    }
+}
+
+record Blueprint(int number, Stock orecost, Stock claycost, Stock obsidiancost, Stock geodecost)
+{
+    public static Blueprint Parse(string line)
+    {
+        string[] parts = line.Split(' ');
+        return new Blueprint(
+            // Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+
+            int.Parse(parts[1].Substring(0, parts[1].Length - 1)),
+            new Stock(int.Parse(parts[6]), 0, 0, 0),
+            new Stock(int.Parse(parts[12]), 0, 0, 0),
+            new Stock(int.Parse(parts[18]), int.Parse(parts[21]), 0, 0),
+            new Stock(int.Parse(parts[27]), 0, int.Parse(parts[30]), 0));
+    }
+}
+
+record State(Stock bots, Stock materials, int time)
+{
+
+}
