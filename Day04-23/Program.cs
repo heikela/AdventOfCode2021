3e289cfbf6339e0d4bbe28197362622f62ded3b3
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-04 05:43:50 +0000

    Day 4 part 2

--- Day04-23/Program.cs
+++ Day04-23/Program.cs
@@ -1,4 +1,4 @@
-﻿//using Common;
+﻿using Common;
 
 //string fileName = "../../../testInput.txt";
 string fileName = "../../../input.txt";
@@ -32,5 +32,23 @@ int part1Points(int matches)
 
 var payout = lines.Select(evaluateCard).Select(part1Points).Sum();
 
+Dictionary<int, long> cardCounts = Enumerable.Range(1, lines.Length).Select(i => new KeyValuePair<int, long>(i, 1)).ToDictionary();
+
+int currentCard = 1;
+foreach (var line in lines)
+{
+    long currentCopies = cardCounts[currentCard];
+    int correct = evaluateCard(line);
+    for (int offset = 1; offset <= correct; ++offset)
+    {
+        int cardToCopy = currentCard + offset;
+        if (cardCounts.ContainsKey(cardToCopy))
+        {
+            cardCounts[cardToCopy] += currentCopies;
+        }
+    }
+    ++currentCard;
+}
+
 Console.WriteLine($"Part 1: {payout}");
-Console.WriteLine($"Part 2:");
+Console.WriteLine($"Part 2: {cardCounts.Values.Sum()}");
