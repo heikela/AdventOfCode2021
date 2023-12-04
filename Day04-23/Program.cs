8808d5ec697c894ad5c85d59080f7fb9f37c94e3
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-04 05:28:49 +0000

    Refactor part 1 in preparation for part 2

--- Day04-23/Program.cs
+++ Day04-23/Program.cs
@@ -10,25 +10,27 @@ int evaluateCard(string line)
     var parts = line.Split('|');
     var correct = parts[0].Split(':')[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse);
     var mine = parts[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse);
+    return mine.Count(n => correct.Contains(n));
+}
+
+int part1Points(int matches)
+{
     var payout = 0;
-    foreach (int n in mine)
+    for (int i = 0; i < matches; ++i)
     {
-        if (correct.Contains(n))
+        if (payout == 0)
         {
-            if (payout == 0)
-            {
-                payout = 1;
-            }
-            else
-            {
-                payout *= 2;
-            }
+            payout = 1;
+        }
+        else
+        {
+            payout *= 2;
         }
     }
     return payout;
 }
 
-var payout = lines.Select(evaluateCard).Sum();
+var payout = lines.Select(evaluateCard).Select(part1Points).Sum();
 
 Console.WriteLine($"Part 1: {payout}");
 Console.WriteLine($"Part 2:");
