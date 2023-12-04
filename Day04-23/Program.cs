aaf15ee1666a07871a7eae83903d3752edb19b5c
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-04 05:23:54 +0000

    Day 4 part 1

--- Day04-23/Program.cs
+++ Day04-23/Program.cs
@@ -1,9 +1,34 @@
-﻿using Common;
+﻿//using Common;
 
 //string fileName = "../../../testInput.txt";
 string fileName = "../../../input.txt";
 
 string[] lines = File.ReadAllLines(fileName).ToArray();
 
-Console.WriteLine($"Part 1:");
+int evaluateCard(string line)
+{
+    var parts = line.Split('|');
+    var correct = parts[0].Split(':')[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse);
+    var mine = parts[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse);
+    var payout = 0;
+    foreach (int n in mine)
+    {
+        if (correct.Contains(n))
+        {
+            if (payout == 0)
+            {
+                payout = 1;
+            }
+            else
+            {
+                payout *= 2;
+            }
+        }
+    }
+    return payout;
+}
+
+var payout = lines.Select(evaluateCard).Sum();
+
+Console.WriteLine($"Part 1: {payout}");
 Console.WriteLine($"Part 2:");
