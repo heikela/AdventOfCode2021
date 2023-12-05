16d978b4355f2c4077b3ed5b41909729f6b3bebd
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-05 05:33:06 +0000

    Day 5 part 1

--- Day05-23/Program.cs
+++ Day05-23/Program.cs
@@ -3,6 +3,69 @@
 //string fileName = "../../../testInput.txt";
 string fileName = "../../../input.txt";
 
-string[] lines = File.ReadAllLines(fileName).ToArray();
+var sections = File.ReadAllLines(fileName).Paragraphs();
 
-Console.WriteLine();
+List<long> seeds = sections.First().First().Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+
+List<Mapping> mappings = new List<Mapping>();
+foreach (var section in sections.Skip(1))
+{
+    Mapping mapping = new Mapping();
+    mappings.Add(mapping);
+    foreach (var line in section.Skip(1))
+    {
+        mapping.AddRangeDefinition(line);
+    }
+}
+
+long SeedToLocation(long seed)
+{
+    return mappings.Aggregate(seed, (n, mapping) => mapping.Transform(n));
+}
+
+Console.WriteLine($"Lowest location number = {seeds.Select(SeedToLocation).Min()}");
+
+public record Range(long Start, long Len)
+{
+    public bool Contains(long n)
+    {
+        return n >= Start && n < Start + Len;
+    }
+    public long TransformFrom(Range Source, long n)
+    {
+        if (!Source.Contains(n))
+        {
+            throw new ArgumentOutOfRangeException("Value to transform not in the given source range");
+        }
+        return n - Source.Start + Start;
+    }
+}
+
+public class Mapping
+{
+    private Dictionary<Range, Range> Ranges = new Dictionary<Range, Range>();
+    public Mapping()
+    {
+    }
+    public void AddRangeDefinition(string line)
+    {
+        List<long> numbers = line.Split(' ').Select(long.Parse).ToList();
+        Range source = new Range(numbers[1], numbers[2]);
+        Range destination = new Range(numbers[0], numbers[2]);
+        Ranges.Add(source, destination);
+    }
+
+    public long Transform(long n)
+    {
+        var applicableMappings = Ranges.Keys.Where(r => r.Contains(n));
+        if (applicableMappings.Any())
+        {
+            Range source = applicableMappings.First();
+            return Ranges[source].TransformFrom(source, n);
+        }
+        else
+        {
+            return n;
+        }
+    }
+}
