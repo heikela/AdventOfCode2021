94d17975e85a9bfa9e794148d03ff8886e9dd3c8
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-05 05:43:09 +0000

    Part 2 brute force attempt

--- Day05-23/Program.cs
+++ Day05-23/Program.cs
@@ -6,6 +6,16 @@ string fileName = "../../../input.txt";
 var sections = File.ReadAllLines(fileName).Paragraphs();
 
 List<long> seeds = sections.First().First().Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+List<long> seeds2 = new List<long>();
+
+for (int i = 0; i < seeds.Count - 1; i += 2)
+{
+    long start = seeds[i];
+    for (long offset = 0; offset < seeds[i + 1]; ++offset)
+    {
+        seeds2.Add(start + offset);
+    }
+}
 
 List<Mapping> mappings = new List<Mapping>();
 foreach (var section in sections.Skip(1))
@@ -24,6 +34,7 @@ long SeedToLocation(long seed)
 }
 
 Console.WriteLine($"Lowest location number = {seeds.Select(SeedToLocation).Min()}");
+Console.WriteLine($"Lowest location number with loads of seeds = {seeds2.Select(SeedToLocation).Min()}");
 
 public record Range(long Start, long Len)
 {
