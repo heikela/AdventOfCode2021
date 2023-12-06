53d9b99a67d9aaf1c0bb75de0905a3fee9e62ac8
Mikko Heikelä <mikko.heikela@gmail.com>
2023-12-06 05:30:29 +0000

    Refactor to use more common code between parts 1 and 2
    And use more Linq instead of explicit loops.
    Probably to the detriment of clarity this time.

--- Day06-23/Program.cs
+++ Day06-23/Program.cs
@@ -1,52 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-List<int> durations = new List<int>() { 47, 84, 74, 67 };
-List<int> records = new List<int>() { 207, 1394, 1209, 1014 };
+List<long> durations = new List<long>() { 47, 84, 74, 67 };
+List<long> records = new List<long>() { 207, 1394, 1209, 1014 };
 
 bool beatsRecord(long charge, long duration, long record)
 {
     return charge * (duration - charge) > record;
 }
 
-int result = 1;
-for (int i = 0; i < durations.Count; ++i)
+long countWaysToWin(long duration, long record)
 {
-    int waysToWin = 0;
-    for (int charge = 1; charge < durations[i]; ++charge)
+    long waysToWin = 0;
+    for (long charge = 1; charge < duration; ++charge)
     {
-        if (beatsRecord(charge,durations[i], records[i]))
+        if (beatsRecord(charge, duration, record))
         {
             ++waysToWin;
         }
     }
-    result *= waysToWin;
+    return waysToWin;
 }
 
-Console.WriteLine(result);
+Console.WriteLine(durations.Zip(records).Select<(long, long), long>(race => countWaysToWin(race.Item1, race.Item2)).Aggregate(1L, (a, b) => a * b));
 
 long duration = 47847467;
 long record = 207139412091014;
 
-/* Realise the numbers are small enough you won't need binary search
- * or explicit solution formula
-
-long firstWinUB = duration;
-long firstWinLB = 0;
-long firstWin = 0;
-
-while (firstWinUB - firstWinLB > 1)
-{
-    long candidate = (firstWinUB + firstWinLB) / 2;
-}*/
-
-long waysToWinPart2 = 0;
-for (long charge = 1; charge < duration; ++charge)
-{
-    if (beatsRecord(charge, duration, record))
-    {
-        ++waysToWinPart2;
-    }
-}
-Console.WriteLine(waysToWinPart2);
-
+Console.WriteLine(countWaysToWin(duration, record));
