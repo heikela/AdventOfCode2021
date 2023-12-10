3fac89dcdc3c1841d8c3f9f297b014f596f35a82
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-10 05:03:56 +0000

    Prepare for day 10

---
+++ Day10-23/Program.cs
@@ -0,0 +1,8 @@
+﻿using Common;
+
+//string fileName = "../../../testInput.txt";
+string fileName = "../../../input.txt";
+
+string[] lines = File.ReadAllLines(fileName).ToArray();
+
+Console.WriteLine();
