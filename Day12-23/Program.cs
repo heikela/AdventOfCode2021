6b6be86d3d872faae9fefa5c4d3c0d72d543b963
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-12 22:10:18 +0000

    Refactor: move most of the logic to a SpringRecord class
    that encapsulates the problem.

--- Day12-23/Program.cs
+++ Day12-23/Program.cs
@@ -15,160 +15,102 @@ Dictionary<IndexPair, long> known2 = new Dictionary<IndexPair, long>();
 long sum1 = 0;
 foreach (var record in records)
 {
-    long result = CountArrangements2(record);
+    long result = record.CountArrangements();
     sum1 += result;
 }
 Console.WriteLine($"Part 1 : {sum1}");
 
-long Memoize2(IndexPair x, long result)
-{
-    known2[x] = result;
-    return result;
-}
-
-bool IsMemoized2(IndexPair x)
-{
-    return known2.ContainsKey(x);
-}
-
-long GetMemoized2(IndexPair x)
-{
-    return known2[x];
-}
-
-void ResetMemo2()
-{
-    known2 = new Dictionary<IndexPair, long>();
-}
-
-long CountArrangementsRec(SpringRecord record, int pos, int runIndex, Dictionary<FastForwardKey, int> nextPossibleRunStart, Dictionary<int, int> lastPossibleStartByRunIndex)
-{
-    IndexPair args = new IndexPair(pos, runIndex);
-    if (IsMemoized2(args))
-    {
-        return GetMemoized2(args);
-    }
-    if (runIndex == record.BrokenRuns.Length)
-    {
-        if (record.IndividualNotes.Substring(Math.Min(pos, record.IndividualNotes.Length)).Any(c => c == '#'))
-        {
-            return Memoize2(args, 0);
-        }
-        else
-        {
-            return Memoize2(args, 1);
-        }
-    }
-    int runLength = record.BrokenRuns[runIndex];
-    if (!nextPossibleRunStart.ContainsKey(new FastForwardKey(pos, runLength)))
-    {
-        return Memoize2(args, 0);
-    }
-    int possibleStart = nextPossibleRunStart[new FastForwardKey(pos, runLength)];
-    if (possibleStart == -1 || possibleStart > lastPossibleStartByRunIndex[runIndex])
-    {
-        return Memoize2(args, 0);
-    }
-    else if (record.IndividualNotes.Substring(pos, possibleStart - pos).Any(c => c == '#'))
-    {
-        return Memoize2(args, 0);
-    }
-    else if (record.IndividualNotes[possibleStart] == '#')
-    {
-        if (record.CanAccommodate(runLength, possibleStart))
-        {
-            return Memoize2(args, CountArrangementsRec(record, possibleStart + runLength + 1, runIndex + 1, nextPossibleRunStart, lastPossibleStartByRunIndex));
-        }
-        else
-        {
-            return Memoize2(args, 0);
-        }
-    }
-    else if (record.IndividualNotes[possibleStart] == '?')
-    {
-        if (record.CanAccommodate(runLength, possibleStart))
-        {
-            long countIfStartHere = CountArrangementsRec(record, possibleStart + runLength + 1, runIndex + 1, nextPossibleRunStart, lastPossibleStartByRunIndex);
-            long countIfNotYet = CountArrangementsRec(record, possibleStart + 1, runIndex, nextPossibleRunStart, lastPossibleStartByRunIndex);
-            return Memoize2(args, countIfStartHere + countIfNotYet);
-        }
-        else
-        {
-            return Memoize2(args, CountArrangementsRec(record, possibleStart + 1, runIndex, nextPossibleRunStart, lastPossibleStartByRunIndex));
-        }
-    }
-    else
-    {
-        throw new Exception($"Unexpected character {record.IndividualNotes[possibleStart]} at supposed possible run start position");
-    }
-}
-
-long CountArrangements2(SpringRecord record)
-{
-    List<int> relevantLengths = record.BrokenRuns.ToHashSet().OrderBy(x => x).ToList();
-    Dictionary<FastForwardKey, int> nextPossibleRunStart = new Dictionary<FastForwardKey, int>();
-    for (int pos = 0; pos < record.IndividualNotes.Length; pos++)
-    {
-        foreach (int runLength in relevantLengths)
-        {
-            int candidate = pos;
-            FastForwardKey key = new FastForwardKey(pos, runLength);
-            IEnumerable<int> possibleStarts = Enumerable.Range(pos, Math.Max(0, record.IndividualNotes.Length - runLength - pos + 1)).Where(pos => record.CanAccommodate(runLength, pos));
-            if (possibleStarts.Any())
-            {
-                nextPossibleRunStart[key] = possibleStarts.First();
-            }
-            else
-            {
-                nextPossibleRunStart[key] = -1;
-            }
-        }
-    }
-
-    Dictionary<int, int> lastPossibleStartByRunIndex = new Dictionary<int, int>();
-    int end = record.IndividualNotes.Length;
-    for (int runIndex = record.BrokenRuns.Length - 1; runIndex >= 0; runIndex--)
-    {
-        int runLength = record.BrokenRuns[runIndex];
-        IEnumerable<int> possibleStarts = Enumerable.Range(0, end - runLength + 1).Reverse().Where(pos => record.CanAccommodate(runLength, pos));
-        int lastPossibleStart = -1;
-        if (possibleStarts.Any())
-        {
-            lastPossibleStart = possibleStarts.First();
-        }
-        end = lastPossibleStart;
-
-        lastPossibleStartByRunIndex[runIndex] = lastPossibleStart;
-    }
-
-    ResetMemo2();
-    return CountArrangementsRec(record, 0, 0, nextPossibleRunStart, lastPossibleStartByRunIndex);
-}
-
 long sum = 0;
 foreach (var record in bigRecords)
 {
-    long result = CountArrangements2(record);
+    long result = record.CountArrangements();
     sum += result;
 }
 Debug.Assert(sum == 157383940585037);
 
 Console.WriteLine("Part 2 result " + sum);
 
-public record SpringRecord(string IndividualNotes, int[] BrokenRuns)
+public class SpringRecord
 {
+    private string IndividualNotes;
+    private List<int> BrokenRuns;
+    private Dictionary<FastForwardKey, int> NextPossibleRunStart;
+    private Dictionary<int, int> LastPossibleStartByRunIndex;
+    private Dictionary<IndexPair, long> MemoizedResult;
+
+    private long Memoize(IndexPair x, long result)
+    {
+        MemoizedResult[x] = result;
+        return result;
+    }
+
+    private bool IsMemoized(IndexPair x)
+    {
+        return MemoizedResult.ContainsKey(x);
+    }
+
+    private long GetMemoized(IndexPair x)
+    {
+        return MemoizedResult[x];
+    }
+
+
+    private SpringRecord(string notes, IEnumerable<int> brokenRuns)
+    {
+        IndividualNotes = notes;
+        BrokenRuns = brokenRuns.ToList();
+
+        List<int> relevantLengths = BrokenRuns.ToHashSet().OrderBy(x => x).ToList();
+        NextPossibleRunStart = new Dictionary<FastForwardKey, int>();
+        for (int pos = 0; pos < IndividualNotes.Length; pos++)
+        {
+            foreach (int runLength in relevantLengths)
+            {
+                int candidate = pos;
+                FastForwardKey key = new FastForwardKey(pos, runLength);
+                IEnumerable<int> possibleStarts = Enumerable.Range(pos, Math.Max(0, IndividualNotes.Length - runLength - pos + 1)).Where(pos => CanAccommodate(runLength, pos));
+                if (possibleStarts.Any())
+                {
+                    NextPossibleRunStart[key] = possibleStarts.First();
+                }
+                else
+                {
+                    NextPossibleRunStart[key] = -1;
+                }
+            }
+        }
+
+        LastPossibleStartByRunIndex = new Dictionary<int, int>();
+        int end = IndividualNotes.Length;
+        for (int runIndex = BrokenRuns.Count - 1; runIndex >= 0; runIndex--)
+        {
+            int runLength = BrokenRuns[runIndex];
+            IEnumerable<int> possibleStarts = Enumerable.Range(0, end - runLength + 1).Reverse().Where(pos => CanAccommodate(runLength, pos));
+            int lastPossibleStart = -1;
+            if (possibleStarts.Any())
+            {
+                lastPossibleStart = possibleStarts.First();
+            }
+            end = lastPossibleStart;
+
+            LastPossibleStartByRunIndex[runIndex] = lastPossibleStart;
+        }
+
+        MemoizedResult = new Dictionary<IndexPair, long>();
+    }
+
     public static SpringRecord Parse(string line)
     {
         var parts = line.Split(" ");
         var notes = parts[0];
-        var brokenRuns = parts[1].Split(",").Select(int.Parse).ToArray();
+        var brokenRuns = parts[1].Split(",").Select(int.Parse);
         return new SpringRecord(notes, brokenRuns);
     }
 
     public SpringRecord FiveFold()
     {
         string repeatedNotes = String.Join("?", Enumerable.Repeat(IndividualNotes, 5));
-        int[] repeatedRuns = Enumerable.Repeat(BrokenRuns, 5).Flatten().ToArray();
+        List<int> repeatedRuns = Enumerable.Repeat(BrokenRuns, 5).Flatten().ToList();
         return new SpringRecord(repeatedNotes, repeatedRuns);
     }
 
@@ -187,6 +129,72 @@ public record SpringRecord(string IndividualNotes, int[] BrokenRuns)
         return $"{IndividualNotes} {String.Join(',', BrokenRuns.Select(n => n.ToString()))}";
     }
 
+    public long CountArrangements()
+    {
+        return CountArrangementsRec(0, 0);
+    }
+
+    long CountArrangementsRec(int pos, int runIndex)
+    {
+        IndexPair args = new IndexPair(pos, runIndex);
+        if (IsMemoized(args))
+        {
+            return GetMemoized(args);
+        }
+        if (runIndex == BrokenRuns.Count)
+        {
+            if (IndividualNotes.Substring(Math.Min(pos, IndividualNotes.Length)).Any(c => c == '#'))
+            {
+                return Memoize(args, 0);
+            }
+            else
+            {
+                return Memoize(args, 1);
+            }
+        }
+        int runLength = BrokenRuns[runIndex];
+        if (!NextPossibleRunStart.ContainsKey(new FastForwardKey(pos, runLength)))
+        {
+            return Memoize(args, 0);
+        }
+        int possibleStart = NextPossibleRunStart[new FastForwardKey(pos, runLength)];
+        if (possibleStart == -1 || possibleStart > LastPossibleStartByRunIndex[runIndex])
+        {
+            return Memoize(args, 0);
+        }
+        else if (IndividualNotes.Substring(pos, possibleStart - pos).Any(c => c == '#'))
+        {
+            return Memoize(args, 0);
+        }
+        else if (IndividualNotes[possibleStart] == '#')
+        {
+            if (CanAccommodate(runLength, possibleStart))
+            {
+                return Memoize(args, CountArrangementsRec(possibleStart + runLength + 1, runIndex + 1));
+            }
+            else
+            {
+                return Memoize(args, 0);
+            }
+        }
+        else if (IndividualNotes[possibleStart] == '?')
+        {
+            if (CanAccommodate(runLength, possibleStart))
+            {
+                long countIfStartHere = CountArrangementsRec(possibleStart + runLength + 1, runIndex + 1);
+                long countIfNotYet = CountArrangementsRec(possibleStart + 1, runIndex);
+                return Memoize(args, countIfStartHere + countIfNotYet);
+            }
+            else
+            {
+                return Memoize(args, CountArrangementsRec(possibleStart + 1, runIndex));
+            }
+        }
+        else
+        {
+            throw new Exception($"Unexpected character {IndividualNotes[possibleStart]} at supposed possible run start position");
+        }
+    }
 }
 
 public record FastForwardKey(int Pos, int RunLength)
