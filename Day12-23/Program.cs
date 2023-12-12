5a0679b1c813aef00a13388f8687824ba6b9ede4
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-12 23:15:21 +0000

    Move the generic part of the dynamic programming solution to the common library

--- Day12-23/Program.cs
+++ Day12-23/Program.cs
@@ -17,48 +17,6 @@ long sum = bigRecords.Sum(r => r.CountArrangements());
 Console.WriteLine($"Part 2 : {sum}");
 Debug.Assert(sum == 157383940585037);
 
-public abstract class DynamicProgrammingProblem<TResult, TSubproblemState>
-{
-    private Dictionary<TSubproblemState, TResult> MemoizedResult;
-
-    protected DynamicProgrammingProblem()
-    {
-        MemoizedResult = new Dictionary<TSubproblemState, TResult>();
-    }
-    private TResult Memoize(TSubproblemState x, TResult result)
-    {
-        MemoizedResult[x] = result;
-        return result;
-    }
-
-    private bool IsMemoized(TSubproblemState x)
-    {
-        return MemoizedResult.ContainsKey(x);
-    }
-
-    private TResult GetMemoized(TSubproblemState x)
-    {
-        return MemoizedResult[x];
-    }
-
-    protected abstract TResult SolveSubproblem(TSubproblemState subProblemState);
-
-    protected TResult SolveAndMemoizeSubproblem(TSubproblemState subProblemState)
-    {
-        if (IsMemoized(subProblemState))
-        {
-            return GetMemoized(subProblemState);
-        }
-        else
-        {
-            TResult result = SolveSubproblem(subProblemState);
-            Memoize(subProblemState, result);
-            return result;
-        }
-    }
-
-}
-
 public class SpringRecord : DynamicProgrammingProblem<long, IndexPair>
 {
     private string IndividualNotes;
