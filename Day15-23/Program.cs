8ca952d44c13fecfd05fa61d0ad45e3135ee88f7
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-15 05:11:13 +0000

    Day 15 part 1

---
+++ Day15-23/Program.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+
+var fileName = "../../../input.txt";
+// var fileName = "../../../testInput.txt";
+
+string line = File.ReadLines(fileName).First();
+
+int hash(string s)
+{
+    int result = 0;
+    foreach (char c in s)
+    {
+        result = ((result + c) * 17 % 256);
+    }
+    return result;
+}
+
+Console.WriteLine($"Part 1 : {line.Split(',').Sum(hash)}");
+
