838f302c74846f8fe26a440a8f7191b00a192dfc
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-15 05:35:58 +0000

    Day 15 part 2 initial solution

--- Day15-23/Program.cs
+++ Day15-23/Program.cs
@@ -1,7 +1,5 @@
-﻿using Common;
-
-var fileName = "../../../input.txt";
-// var fileName = "../../../testInput.txt";
+﻿var fileName = "../../../input.txt";
+//var fileName = "../../../testInput.txt";
 
 string line = File.ReadLines(fileName).First();
 
@@ -16,4 +14,54 @@ int hash(string s)
 }
 
 Console.WriteLine($"Part 1 : {line.Split(',').Sum(hash)}");
+
+List<List<KeyValuePair<string, int>>> boxes = Enumerable.Range(0, 256).Select(i => new List<KeyValuePair<string, int>>()).ToList();
+
+foreach (string step in line.Split(','))
+{
+    char[] operators = { '=', '-' };
+    string[] parts = step.Split(operators);
+    string name = parts[0];
+    int lens = 0;
+    char op = step[name.Length];
+    if ( op == '=')
+    {
+        lens = int.Parse(step.Substring(name.Length + 1));
+    }
+    int box = hash(name);
+    if (op == '-')
+    {
+        boxes[box].RemoveAll(kv => kv.Key == name);
+    }
+    if (op == '=')
+    {
+        int existingIndex = boxes[box].FindIndex(kv => kv.Key == name);
+        if (existingIndex >= 0)
+        {
+            boxes[box][existingIndex] = new KeyValuePair<string, int>(name, lens);
+        }
+        else
+        {
+            boxes[box].Add(new KeyValuePair<string, int>(name, lens));
+        }
+    }
+}
+
+int power(int boxIndex, List<KeyValuePair<string, int>> contents)
+{
+    int result = 0;
+    for (int i = 0; i < contents.Count; ++i)
+    {
+        result += (boxIndex + 1) * (i + 1) * contents[i].Value;
+    }
+    return result;
+}
+
+int part2 = 0;
+for (int i = 0; i < 256; ++i)
+{
+    part2 += power(i, boxes[i]);
+}
+
+Console.WriteLine($"Part 2 : {part2}");
 
