f10966052ff1165d4e75f07b5696b29a49dcc9fb
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-23 05:57:10 +0000

    Day 22 and 23 initial solutions

---
+++ Day22-23/Program.cs
@@ -0,0 +1,156 @@
+﻿using Common;
+
+string fileName = "../../../input.txt";
+//string fileName = "../../../testInput.txt";
+
+Point down = new Point(0, 0, -1);
+Point up = new Point(0, 0, 1);
+
+var lines = File.ReadAllLines(fileName);
+
+var bricks = lines.Select(line =>
+{
+    var parts = line.Split('~');
+    var a = parts[0].Split(',').Select(int.Parse).ToArray();
+    var b = parts[1].Split(',').Select(int.Parse).ToArray();
+    return new Brick(new Point(a[0], a[1], a[2]), new Point(b[0], b[1], b[2]));
+}).OrderBy(b => b.MinZ()).ToArray();
+
+Dictionary<Point, int> brickByPos = new Dictionary<Point, int>();
+
+for (int i = 0; i < bricks.Length; ++i)
+{
+    Brick b = bricks[i];
+    foreach (Point p in b.GetPoints())
+    {
+        brickByPos.Add(p, i);
+    }
+}
+
+bool canFall(Brick brick)
+{
+    if (brick.MinZ() == 1)
+    {
+        return false;
+    }
+    return brick.GetSupport().All(p => !brickByPos.ContainsKey(p));
+}
+
+for (int i = 0; i < bricks.Length; ++i)
+{
+    Brick b = bricks[i];
+    while (canFall(b))
+    {
+        Brick newB = b.MoveDown();
+        bricks[i] = newB;
+        foreach (Point p in b.GetTop())
+        {
+            brickByPos.Remove(p);
+        }
+        foreach (Point p in b.GetSupport())
+        {
+            brickByPos.Add(p, i);
+        }
+        b = newB;
+    }
+}
+
+HashSet<int> supporters(Brick brick)
+{
+    return brick.GetSupport().Where(p => brickByPos.ContainsKey(p)).Select(p => brickByPos[p]).ToHashSet();
+}
+
+bool canBeDisintegrated(Brick brick)
+{
+    return brick.GetAbove().All(p => !brickByPos.ContainsKey(p) || supporters(bricks[brickByPos[p]]).Count > 1);
+}
+
+int canBeDisintegratedIndividually = bricks.Count(canBeDisintegrated);
+
+Console.WriteLine($"Part 1: {canBeDisintegratedIndividually}");
+
+int countFallingIfDisintegrated(int idx)
+{
+    HashSet<int> disturbed = new HashSet<int>();
+    disturbed.Add(idx);
+    for (int i = idx; i < bricks.Length; ++i)
+    {
+        Brick b = bricks[i];
+        HashSet<int> bricksAbove = b.GetAbove().Where(p => brickByPos.ContainsKey(p)).Select(p => brickByPos[p]).ToHashSet();
+        foreach (int brickAbove in bricksAbove)
+        {
+            if (supporters(bricks[brickAbove]).All(i => disturbed.Contains(i)))
+            {
+                disturbed.Add(brickAbove);
+            }
+        }
+    }
+    return disturbed.Count - 1;
+}
+
+int fallingSum = 0;
+for (int i = 0; i < bricks.Length; ++i)
+{
+    fallingSum += countFallingIfDisintegrated(i);
+}
+
+Console.WriteLine(fallingSum);
+
+public record Brick(Point A, Point B)
+{
+    public IEnumerable<Point> GetPoints()
+    {
+        int MinX = Math.Min(A.X, B.X);
+        int MaxX = Math.Max(A.X, B.X);
+        int MinY = Math.Min(A.Y, B.Y);
+        int MaxY = Math.Max(A.Y, B.Y);
+        int MinZ = Math.Min(A.Z, B.Z);
+        int MaxZ = Math.Max(A.Z, B.Z);
+
+        for (int x = MinX; x <= MaxX; x++)
+        {
+            for (int y = MinY; y <= MaxY; y++)
+            {
+                for (int z = MinZ; z <= MaxZ; z++)
+                {
+                    yield return new Point(x, y, z);
+                }
+            }
+        }
+    }
+
+    public IEnumerable<Point> GetSupport()
+    {
+        HashSet<Point> current = GetPoints().ToHashSet();
+        return current.Select(p => p + new Point(0, 0, -1)).Where(p => !current.Contains(p));
+    }
+
+    public IEnumerable<Point> GetAbove()
+    {
+        HashSet<Point> current = GetPoints().ToHashSet();
+        return current.Select(p => p + new Point(0, 0, 1)).Where(p => !current.Contains(p));
+    }
+
+    public IEnumerable<Point> GetTop()
+    {
+        HashSet<Point> current = GetPoints().ToHashSet();
+        return current.Where(p => !current.Contains(p + new Point(0, 0, 1)));
+    }
+
+    public Brick MoveDown()
+    {
+        return new Brick(A + new Point(0, 0, -1), B + new Point(0, 0, -1));
+    }
+
+    public int MinZ()
+    {
+        return Math.Min(A.Z, B.Z);
+    }
+
+}
+
+public record Point(int X, int Y, int Z)
+{
+    public static Point operator+(Point a, Point b) => new Point(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+}
+
---
+++ Day23-23/Program.cs
@@ -0,0 +1,173 @@
+﻿using Common;
+
+var fileName = "../../../input.txt";
+// var fileName = "../../../testInput.txt";
+
+var lines = File.ReadAllLines(fileName);
+
+Point up = new Point(0, -1);
+Point down = new Point(0, 1);
+Point left = new Point(-1, 0);
+Point right = new Point(1, 0);
+
+Point[] directions = new Point[] { up, down, left, right };
+
+Dictionary<Point, char> map = new Dictionary<Point, char>();
+
+for (int y = 0; y < lines.Length; ++y)
+{
+    string line = lines[y];
+    for (int x = 0; x < line.Length; ++x)
+    {
+        map.Add(new Point(x, y), line[x]);
+    }
+}
+
+Point start = map.Where(kv => kv.Key.Y == 0 && kv.Value == '.').Single().Key;
+Point end = map.Where(kv => kv.Key.Y == lines.Length - 1 && kv.Value == '.').Single().Key;
+
+IEnumerable<Point> GetNext1(Point p)
+{
+    char current = map[p];
+    if (current == '.')
+    {
+        foreach (var dir in directions)
+        {
+            Point next = p + dir;
+            if (map.GetOrElse(next, '#') != '#')
+            {
+                yield return next;
+            }
+        }
+    }
+    else if (current == '<')
+    {
+        yield return p + left;
+    }
+    else if (current == '>')
+    {
+        yield return p + right;
+    }
+    else if (current == '^')
+    {
+        yield return p + up;
+    }
+    else if (current == 'v')
+    {
+        yield return p + down;
+    }
+}
+
+IEnumerable<Point> GetNext2(Point p)
+{
+    char current = map[p];
+    foreach (var dir in directions)
+    {
+        Point next = p + dir;
+        if (map.GetOrElse(next, '#') != '#')
+        {
+            yield return next;
+        }
+    }
+}
+
+int longest1 = 0;
+void explore1(Point current, HashSet<Point> visited, int length)
+{
+    if (current == end)
+    {
+        longest1 = Math.Max(longest1, length);
+        return;
+    }
+    HashSet<Point> newVisited = new HashSet<Point>(visited);
+    newVisited.Add(current);
+    foreach (var next in GetNext1(current))
+    {
+        if (visited.Contains(next))
+        {
+            continue;
+        }
+        explore1(next, newVisited, length + 1);
+    }
+}
+
+//explore1(start, new HashSet<Point>(), 0);
+
+Dictionary<Point, List<(Point, int)>> nextJunctions = new Dictionary<Point, List<(Point, int)>>();
+HashSet<Point> doneDirections = new HashSet<Point>();
+
+void addPath(Point start, Point end, int length)
+{
+    if (!nextJunctions.ContainsKey(start))
+    {
+        nextJunctions[start] = new List<(Point, int)>();
+    }
+    nextJunctions[start].Add((end, length));
+    if (!nextJunctions.ContainsKey(end))
+    {
+        nextJunctions[end] = new List<(Point, int)>();
+    }
+    nextJunctions[end].Add((start, length));
+}
+
+
+void createPart2Map(Point current, Point prev)
+{
+    Point pathStart = prev;
+    int length = 1;
+    while (true)
+    {
+        List<Point> connectedPoints = GetNext2(current).ToList();
+        if (connectedPoints.Count > 2 || current == end)
+        {
+            addPath(pathStart, current, length);
+            doneDirections.Add(prev);
+            foreach (Point newStart in connectedPoints.Where(p => !doneDirections.Contains(p)))
+            {
+                doneDirections.Add(newStart);
+                createPart2Map(newStart, current);
+            }
+            break;
+        }
+        if (connectedPoints.Count <= 1)
+        {
+            break;
+        }
+        Point next = connectedPoints.Single(p => p != prev);
+        prev = current;
+        current = next;
+        length++;
+    }
+}
+
+int longest2 = 0;
+void explore2(Point current, HashSet<Point> visited, int length)
+{
+    if (current == end)
+    {
+        longest2 = Math.Max(longest2, length);
+        return;
+    }
+    HashSet<Point> newVisited = new HashSet<Point>(visited);
+    newVisited.Add(current);
+    foreach (var (next, dist) in nextJunctions[current])
+    {
+        if (visited.Contains(next))
+        {
+            continue;
+        }
+        explore2(next, newVisited, length + dist);
+    }
+}
+
+createPart2Map(start + down, start);
+explore2(start, new HashSet<Point>(), 0);
+
+Console.WriteLine($"Part 1: {longest1}");
+Console.WriteLine($"Part 2: {longest2}");
+// not 6322
+
+public record Point(int X, int Y)
+{
+    public static Point operator+(Point a, Point b) => new Point(a.X + b.X, a.Y + b.Y);
+}
