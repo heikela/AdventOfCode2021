5d5b56d44e29b2b558f2de6b3054929f404fcbf9
Mikko Heikela <mikko.heikela@gmail.com>
2023-12-25 06:01:37 +0000

    Day 25 and year 2023 completed as far as initial solutions go

--- Common/Graph.cs
+++ Common/Graph.cs
@@ -149,6 +149,7 @@ namespace Common
             }
             yield break;
         }
+
     }
 
     public class ConcreteGraph<T> : Graph<T>
@@ -158,6 +159,11 @@ namespace Common
         public ConcreteGraph()
         {
             Edges = new Dictionary<T, List<T>>();
+        }
+
+        public ConcreteGraph(Dictionary<T, List<T>> edges)
+        {
+            Edges = edges;
         }
 
         public override IEnumerable<T> GetNodes()
@@ -198,6 +204,92 @@ namespace Common
             }
             return reverse;
         }
+
+        public IEnumerable<List<T>> KargerMinimumCut(int goodenough = 3)
+        {
+            int minCut = int.MaxValue;
+            Dictionary<T, T> originalNodesInMinCut = null;
+
+            Random random = new Random();
+
+            int nodeCount = GetNodes().Count();
+            int ln = (int)Math.Log(nodeCount);
+            int iterations = nodeCount * nodeCount * ln;
+
+            int iteration = 0;
+            while (minCut > goodenough && iteration < iterations)
+            {
+                iteration += 1;
+                ConcreteGraph<T> contracted = new ConcreteGraph<T>(Edges.Select(kv => new KeyValuePair<T, List<T>>(kv.Key, kv.Value.ToList())).ToDictionary());
+                Dictionary<T, T> originalNodes = new Dictionary<T, T>();
+                while (contracted.GetNodes().Count() > 2)
+                {
+                    T[] nodes = contracted.GetNodes().ToArray();
+                    T node1 = nodes[random.Next(nodes.Length)];
+                    T[] neighbours = contracted.GetNeighbours(node1).ToArray();
+                    T node2 = neighbours[random.Next(neighbours.Length)];
+                    originalNodes.Add(node2, node1);
+                    contracted.ContractEdge(node1, node2);
+                }
+                int cut = contracted.GetNeighbours(contracted.GetNodes().First()).Count();
+                if (cut < minCut)
+                {
+                    minCut = cut;
+                    originalNodesInMinCut = originalNodes.ToDictionary();
+                }
+            }
+            Dictionary<T, List<T>> halves = new Dictionary<T, List<T>>();
+            foreach (T node in GetNodes())
+            {
+                T current = node;
+                while (originalNodesInMinCut.ContainsKey(current))
+                {
+                    current = originalNodesInMinCut[current];
+                }
+                if (halves.ContainsKey(current))
+                {
+                    halves[current].Add(node);
+                }
+                else
+                {
+                    halves.Add(current, new List<T>() { node });
+                }
+            }
+            return halves.Values;
+        }
+
+        private void ContractEdge(T node1, T node2)
+        {
+            List<T> newNeighbours = new List<T>();
+            foreach (T neighbour in GetNeighbours(node1))
+            {
+                if (!neighbour.Equals(node2))
+                {
+                    newNeighbours.Add(neighbour);
+                }
+            }
+            foreach (T neighbour in GetNeighbours(node2))
+            {
+                if (!neighbour.Equals(node1))
+                {
+                    newNeighbours.Add(neighbour);
+                }
+            }
+            Edges[node1] = newNeighbours;
+            Edges.Remove(node2);
+            foreach (T neighbour in Edges[node1].ToHashSet())
+            {
+                List<T> neighbours = Edges[neighbour];
+                for (int i = 0; i < neighbours.Count; i++)
+                {
+                    if (neighbours[i].Equals(node2))
+                    {
+                        neighbours[i] = node1;
+                    }
+                }
+            }
+        }
+
     }
 
     public class GraphByFunction<T> : Graph<T> where T : IEquatable<T>
---
+++ Day25-23/Program.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+
+string fileName = "../../../input.txt";
+//string fileName = "../../../testInput.txt";
+
+string[] lines = File.ReadAllLines(fileName);
+
+ConcreteGraph<string> graph = new ConcreteGraph<string>();
+
+foreach (string line in lines)
+{
+    char[] separators = { ' ', ':' };
+    string[] parts = line.Split(separators, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    foreach (string dest in parts.Skip(1))
+    {
+        graph.AddEdge(parts[0], dest);
+        graph.AddEdge(dest, parts[0]);
+    }
+}
+
+List<List<string>> halves = graph.KargerMinimumCut(3).ToList();
+
+Console.WriteLine($"Min cut: {halves[0].Count * halves[1].Count}");
